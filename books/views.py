e4113a48f1a3f98996fbad082453d5c8d34d1ac5
Rupesh Virani <rupeshvirani01@gmail.com>
2024-09-05 21:12:18 +0530

    created book app and implemented jwt auth

---
+++ books/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+# Create your views here.
+
+class Books(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+    
