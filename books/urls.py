9f02f7df6b9421c52c423e649981c65585ae95f0
Rupesh Virani <rupeshvirani01@gmail.com>
2024-09-07 22:03:44 +0530

    created login API View

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Books
+from .views import Books, RegisterApi, LoginApi
 
 urlpatterns = [
     path('',Books.as_view()),
+    path('api/register/',RegisterApi.as_view(), name='register'),
+    path('api/login/', LoginApi.as_view(), name='login'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,8 +1,12 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from .serializer import RegisterSerializer, UserSerializer, LoginSerializer
 
 # Create your views here.
 
@@ -13,4 +17,42 @@ class Books(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
-    
+
+
+class RegisterApi(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginApi(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        print(serializer)
+        print(serializer.is_valid())
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            user = authenticate(username=username,password=password)
+            print(user)
+            if user:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                    'user_id': user.id,
+                    'username': user.username
+                })
+            else:
+                return Response({'error': 'invalid credentials'},status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
